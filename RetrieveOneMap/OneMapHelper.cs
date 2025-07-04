11b3aacc1872d85e2adbeb9648a80fe934b2600e
Rizky <defrianda.r@gitlab.com>
2025-07-04 13:18:20 +0700

    remove unused

--- RetrieveOneMap/OneMapHelper.cs
+++ RetrieveOneMap/OneMapHelper.cs
@@ -1,11 +1,7 @@
 ﻿using ClosedXML.Excel;
 using Serilog;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Text.Json;
-using System.Threading.Tasks;
 
 namespace RetrieveOneMap
 {
